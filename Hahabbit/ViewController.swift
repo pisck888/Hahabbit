3cc4c651c3a87a64a544f58858a8157e3ed3acfc
pisck <pisck780527@hotmail.com>
2021-05-12 01:02:09 +0800

    Add gitignore and import SwiftLint

---
+++ Hahabbit/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Hahabbit
+//
+//  Created by TSAI TSUNG-HAN on 2021/5/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+}
