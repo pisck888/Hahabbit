e75761abec44a0625f756f718ef6d277311397d9
pisck <pisck780527@hotmail.com>
2021-05-13 01:42:16 +0800

    Completed add new goal page basic UI

---
+++ Hahabbit/Resource/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  Hahabbit
+//
+//  Created by TSAI TSUNG-HAN on 2021/5/12.
+//
+
+import Foundation
---
+++ Hahabbit/View/AddNewGoalCell/AddNewGoalCell.swift
@@ -0,0 +1,23 @@
+//
+//  AddNewGoalCell.swift
+//  Hahabbit
+//
+//  Created by TSAI TSUNG-HAN on 2021/5/12.
+//
+
+import UIKit
+
+class AddNewGoalCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
